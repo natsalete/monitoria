f5006c36c40cd31e38acd910e67b5caf1d345873
natalete <natsalete14@gmail.com>
2025-08-13 09:31:38 -0300

    fix:ajuste no envio da monitoria

--- script.js
+++ script.js
@@ -1,182 +1,237 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('monitoria-form');
-    const statusMessage = document.getElementById('status-message');
-    const diaSelecionadoInput = document.getElementById('dia-selecionado');
-    const calendarContainer = document.getElementById('calendar-container');
-    const cursoSelect = document.getElementById('curso');
-    const outrosCursoContainer = document.getElementById('outros-curso-container');
-    const periodoSelect = document.getElementById('periodo');
-    const outrosPeriodoContainer = document.getElementById('outros-periodo-container');
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("monitoria-form");
+  const statusMessage = document.getElementById("status-message");
+  const diaSelecionadoInput = document.getElementById("dia-selecionado");
+  const calendarContainer = document.getElementById("calendar-container");
+  const cursoSelect = document.getElementById("curso");
+  const outrosCursoContainer = document.getElementById(
+    "outros-curso-container"
+  );
+  const periodoSelect = document.getElementById("periodo");
+  const outrosPeriodoContainer = document.getElementById(
+    "outros-periodo-container"
+  );
 
-    const meuWhatsApp = '5534997306154'; 
+  const meuWhatsApp = "5534997306154";
 
-    const availableDaysOfWeek = [1, 3, 5]; // 0=Domingo, 1=Segunda, 2=Terça, etc.
-    const availableHours = {
-        1: '11h às 13h', // Segunda
-        3: '11h às 13h', // Quarta
-        5: '19h às 21h', // Sexta
-    };
+  const availableDaysOfWeek = [1, 3, 5]; // 0=Domingo, 1=Segunda, 2=Terça, etc.
+  const availableHours = {
+    1: "11h às 13h", // Segunda
+    3: "11h às 13h", // Quarta
+    5: "19h às 21h", // Sexta
+  };
 
-    let currentMonth = new Date().getMonth();
-    let currentYear = new Date().getFullYear();
+  let currentMonth = new Date().getMonth();
+  let currentYear = new Date().getFullYear();
 
-    diaSelecionadoInput.addEventListener('focus', () => {
-        calendarContainer.style.display = 'block';
-        renderCalendar(currentMonth, currentYear);
-    });
+  diaSelecionadoInput.addEventListener("focus", () => {
+    calendarContainer.style.display = "block";
+    renderCalendar(currentMonth, currentYear);
+  });
 
-    document.addEventListener('click', (event) => {
-        if (!calendarContainer.contains(event.target) && event.target !== diaSelecionadoInput) {
-            calendarContainer.style.display = 'none';
-        }
-    });
+  document.addEventListener("click", (event) => {
+    if (
+      !calendarContainer.contains(event.target) &&
+      event.target !== diaSelecionadoInput
+    ) {
+      calendarContainer.style.display = "none";
+    }
+  });
 
-    cursoSelect.addEventListener('change', (event) => {
-        if (event.target.value === 'Outro') {
-            outrosCursoContainer.style.display = 'flex';
-        } else {
-            outrosCursoContainer.style.display = 'none';
-        }
-    });
+  cursoSelect.addEventListener("change", (event) => {
+    if (event.target.value === "Outro") {
+      outrosCursoContainer.style.display = "flex";
+    } else {
+      outrosCursoContainer.style.display = "none";
+    }
+  });
 
-    periodoSelect.addEventListener('change', (event) => {
-        if (event.target.value === 'Outro') {
-            outrosPeriodoContainer.style.display = 'flex';
-        } else {
-            outrosPeriodoContainer.style.display = 'none';
-        }
-    });
+  periodoSelect.addEventListener("change", (event) => {
+    if (event.target.value === "Outro") {
+      outrosPeriodoContainer.style.display = "flex";
+    } else {
+      outrosPeriodoContainer.style.display = "none";
+    }
+  });
 
-    function renderCalendar(month, year) {
-        calendarContainer.innerHTML = ''; 
-        const date = new Date(year, month);
-        const firstDayOfMonth = new Date(year, month, 1).getDay();
-        const daysInMonth = new Date(year, month + 1, 0).getDate();
-        const monthNames = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
-        const dayNames = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
-        const today = new Date();
+  function renderCalendar(month, year) {
+    calendarContainer.innerHTML = "";
+    const date = new Date(year, month);
+    const firstDayOfMonth = new Date(year, month, 1).getDay();
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    const monthNames = [
+      "Janeiro",
+      "Fevereiro",
+      "Março",
+      "Abril",
+      "Maio",
+      "Junho",
+      "Julho",
+      "Agosto",
+      "Setembro",
+      "Outubro",
+      "Novembro",
+      "Dezembro",
+    ];
+    const dayNames = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
+    const today = new Date();
 
-        const header = document.createElement('div');
-        header.className = 'calendar-header';
-        header.innerHTML = `
+    const header = document.createElement("div");
+    header.className = "calendar-header";
+    header.innerHTML = `
             <button type="button" class="prev-month"> &lt; </button>
             <h4>${monthNames[month]} ${year}</h4>
             <button type="button" class="next-month"> &gt; </button>
         `;
-        calendarContainer.appendChild(header);
+    calendarContainer.appendChild(header);
 
-        const daysGrid = document.createElement('div');
-        daysGrid.className = 'calendar-days';
-        dayNames.forEach(day => {
-            const dayName = document.createElement('div');
-            dayName.textContent = day;
-            daysGrid.appendChild(dayName);
-        });
-        calendarContainer.appendChild(daysGrid);
+    const daysGrid = document.createElement("div");
+    daysGrid.className = "calendar-days";
+    dayNames.forEach((day) => {
+      const dayName = document.createElement("div");
+      dayName.textContent = day;
+      daysGrid.appendChild(dayName);
+    });
+    calendarContainer.appendChild(daysGrid);
 
-        const datesGrid = document.createElement('div');
-        datesGrid.className = 'calendar-dates';
-        
-        for (let i = 0; i < firstDayOfMonth; i++) {
-            datesGrid.innerHTML += '<div></div>';
-        }
+    const datesGrid = document.createElement("div");
+    datesGrid.className = "calendar-dates";
 
-        for (let i = 1; i <= daysInMonth; i++) {
-            const dayElement = document.createElement('div');
-            const dayDate = new Date(year, month, i);
-            const dayOfWeek = dayDate.getDay();
-
-            dayElement.textContent = i;
-            
-            if (availableDaysOfWeek.includes(dayOfWeek) && (dayDate >= today || (dayDate.toDateString() === today.toDateString()))) {
-                dayElement.className = 'available-day';
-                dayElement.setAttribute('data-date', dayDate.toISOString());
-            } else {
-                dayElement.className = 'inactive-day';
-            }
-            datesGrid.appendChild(dayElement);
-        }
-        calendarContainer.appendChild(datesGrid);
-
-        document.querySelector('.prev-month').addEventListener('click', (e) => {
-            e.stopPropagation(); // Impede que o calendário feche
-            currentMonth--;
-            if (currentMonth < 0) {
-                currentMonth = 11;
-                currentYear--;
-            }
-            renderCalendar(currentMonth, currentYear);
-        });
-
-        document.querySelector('.next-month').addEventListener('click', (e) => {
-            e.stopPropagation(); // Impede que o calendário feche
-            currentMonth++;
-            if (currentMonth > 11) {
-                currentMonth = 0;
-                currentYear++;
-            }
-            renderCalendar(currentMonth, currentYear);
-        });
-
-        datesGrid.addEventListener('click', (event) => {
-            const selectedDayElement = event.target;
-            if (selectedDayElement.classList.contains('available-day')) {
-                const dayOfWeek = new Date(selectedDayElement.getAttribute('data-date')).getDay();
-                const selectedDate = new Date(selectedDayElement.getAttribute('data-date'));
-                const day = selectedDate.getDate();
-                const monthName = monthNames[selectedDate.getMonth()];
-                const dayName = dayNames[dayOfWeek];
-
-                const fullDate = `${dayName}, ${day} de ${monthName} - ${availableHours[dayOfWeek]}`;
-                diaSelecionadoInput.value = fullDate;
-
-                document.querySelectorAll('.available-day').forEach(day => day.classList.remove('selected-day'));
-                
-                selectedDayElement.classList.add('selected-day');
-
-                setTimeout(() => {
-                    calendarContainer.style.display = 'none';
-                }, 500);
-            }
-        });
+    for (let i = 0; i < firstDayOfMonth; i++) {
+      datesGrid.innerHTML += "<div></div>";
     }
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
+    for (let i = 1; i <= daysInMonth; i++) {
+      const dayElement = document.createElement("div");
+      const dayDate = new Date(year, month, i);
+      const dayOfWeek = dayDate.getDay();
 
-        const diaSelecionado = diaSelecionadoInput.value;
-        const nomeParticipante = document.getElementById('nome-participante').value;
-        let curso = cursoSelect.value;
-        if (curso === 'Outro') {
-            curso = document.getElementById('outros-curso').value;
-        }
-        let periodo = periodoSelect.value;
-        if (periodo === 'Outro') {
-            periodo = document.getElementById('outros-periodo').value;
-        }
-        const materia = document.getElementById('materia').value;
+      dayElement.textContent = i;
 
-        if (!diaSelecionado || !nomeParticipante || !curso || !periodo || !materia) {
-            statusMessage.textContent = 'Por favor, preencha todos os campos do formulário.';
-            statusMessage.className = 'status-message error';
-            return;
-        }
+      if (
+        availableDaysOfWeek.includes(dayOfWeek) &&
+        (dayDate >= today || dayDate.toDateString() === today.toDateString())
+      ) {
+        dayElement.className = "available-day";
+        dayElement.setAttribute("data-date", dayDate.toISOString());
+      } else {
+        dayElement.className = "inactive-day";
+      }
+      datesGrid.appendChild(dayElement);
+    }
+    calendarContainer.appendChild(datesGrid);
 
-        const mensagem = `Olá Natalia Salete, sou ${nomeParticipante} e gostaria de agendar uma monitoria.\n\n` +
-                         `*Dados do Agendamento:*\n` +
-                         `Curso: ${curso}\n` +
-                         `Período: ${periodo}\n` +
-                         `Matéria: ${materia}\n` +
-                         `Dia/Horário: ${diaSelecionado}\n\n` +
-                         `*Link da Reunião:*\n` +
-                         `https://meet.google.com/jjj-cfmm-cia\n\n` +
-                         `Por favor, me confirme o agendamento.`;
-
-        const whatsappLink = `https://api.whatsapp.com/send?phone=${meuWhatsApp}&text=${encodeURIComponent(mensagem)}`;
-
-        window.open(whatsappLink, '_blank');
-
-        statusMessage.textContent = 'Agendamento pronto para ser enviado! Uma nova janela do WhatsApp foi aberta.';
-        statusMessage.className = 'status-message success';
+    document.querySelector(".prev-month").addEventListener("click", (e) => {
+      e.stopPropagation(); // Impede que o calendário feche
+      currentMonth--;
+      if (currentMonth < 0) {
+        currentMonth = 11;
+        currentYear--;
+      }
+      renderCalendar(currentMonth, currentYear);
     });
+
+    document.querySelector(".next-month").addEventListener("click", (e) => {
+      e.stopPropagation(); // Impede que o calendário feche
+      currentMonth++;
+      if (currentMonth > 11) {
+        currentMonth = 0;
+        currentYear++;
+      }
+      renderCalendar(currentMonth, currentYear);
+    });
+
+    datesGrid.addEventListener("click", (event) => {
+      const selectedDayElement = event.target;
+      if (selectedDayElement.classList.contains("available-day")) {
+        const dayOfWeek = new Date(
+          selectedDayElement.getAttribute("data-date")
+        ).getDay();
+        const selectedDate = new Date(
+          selectedDayElement.getAttribute("data-date")
+        );
+        const day = selectedDate.getDate();
+        const monthName = monthNames[selectedDate.getMonth()];
+        const dayName = dayNames[dayOfWeek];
+
+        const fullDate = `${dayName}, ${day} de ${monthName} - ${availableHours[dayOfWeek]}`;
+        diaSelecionadoInput.value = fullDate;
+
+        document
+          .querySelectorAll(".available-day")
+          .forEach((day) => day.classList.remove("selected-day"));
+
+        selectedDayElement.classList.add("selected-day");
+
+        setTimeout(() => {
+          calendarContainer.style.display = "none";
+        }, 500);
+      }
+    });
+  }
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const diaSelecionado = diaSelecionadoInput.value;
+    const nomeParticipante = document.getElementById("nome-participante").value;
+    let curso = cursoSelect.value;
+    if (curso === "Outro") {
+      curso = document.getElementById("outros-curso").value;
+    }
+    let periodo = periodoSelect.value;
+    if (periodo === "Outro") {
+      periodo = document.getElementById("outros-periodo").value;
+    }
+    const materia = document.getElementById("materia").value;
+
+    if (
+      !diaSelecionado ||
+      !nomeParticipante ||
+      !curso ||
+      !periodo ||
+      !materia
+    ) {
+      statusMessage.textContent =
+        "Por favor, preencha todos os campos do formulário.";
+      statusMessage.className = "status-message error";
+      return;
+    }
+
+    const mensagem =
+      `Olá Natalia Salete, sou ${nomeParticipante} e gostaria de agendar uma monitoria.\n\n` +
+      `*Dados do Agendamento:*\n` +
+      `Curso: ${curso}\n` +
+      `Período: ${periodo}\n` +
+      `Matéria: ${materia}\n` +
+      `Dia/Horário: ${diaSelecionado}\n\n` +
+      `*Link da Reunião:*\n` +
+      `https://meet.google.com/jjj-cfmm-cia\n\n` +
+      `Por favor, me confirme o agendamento.`;
+
+    // Verifica se é um dispositivo móvel
+    const isMobile =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      );
+
+    let whatsappLink = "";
+    if (isMobile) {
+      // Usa o link otimizado para abrir o app no celular
+      whatsappLink = `whatsapp://send?phone=${meuWhatsApp}&text=${encodeURIComponent(
+        mensagem
+      )}`;
+    } else {
+      // Usa a API web para PC
+      whatsappLink = `https://api.whatsapp.com/send?phone=${meuWhatsApp}&text=${encodeURIComponent(
+        mensagem
+      )}`;
+    }
+
+    window.open(whatsappLink, "_blank");
+
+    statusMessage.textContent =
+      "Agendamento pronto para ser enviado! Uma nova janela do WhatsApp foi aberta.";
+    statusMessage.className = "status-message success";
+  });
 });
